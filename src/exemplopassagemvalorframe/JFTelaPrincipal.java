80db43457624eee854ce066748046203b16f4e97
Hugo Aguiar Martinez Avellan <martinez.avellan@hotmail.com>
2019-07-05 14:53:59 -0300

    commit dos códigos com leitura de arquivo json para as perguntas e remoção da lista

--- src/exemplopassagemvalorframe/JFTelaPrincipal.java
+++ src/exemplopassagemvalorframe/JFTelaPrincipal.java
@@ -1,7 +1,14 @@
 package exemplopassagemvalorframe;
 
 import java.awt.EventQueue;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JFTelaPrincipal extends JFrame {
 
@@ -16,7 +23,8 @@ public class JFTelaPrincipal extends JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Chamar outra tela");
+        jButton1.setText("Iniciar");
+        jButton1.setActionCommand("Iniciar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -28,24 +36,35 @@ public class JFTelaPrincipal extends JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(137, 137, 137)
+                .addGap(166, 166, 166)
                 .addComponent(jButton1)
-                .addContainerGap(144, Short.MAX_VALUE))
+                .addContainerGap(173, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(139, 139, 139)
+                .addGap(135, 135, 135)
                 .addComponent(jButton1)
-                .addContainerGap(138, Short.MAX_VALUE))
+                .addContainerGap(142, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        pulo--;
-        new JFTelaSecundaria(pulo).setVisible(true);        
+
+        try {
+            pulo--;
+            JSONParser parser = new JSONParser();
+            String file = this.getClass().getResource("data.json").getFile();
+            Object obj = parser.parse(new FileReader(file));
+            final JSONArray asks = (JSONArray) obj;
+            
+            new JFTelaSecundaria(pulo, asks).setVisible(true); 
+        } catch (IOException | ParseException e) {
+            Logger.getLogger(JFTelaPrincipal.class.getName()).log(Level.SEVERE, e.getMessage());
+        }
+               
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public static void main(String args[]) {
--- src/exemplopassagemvalorframe/JFTelaSecundaria.java
+++ src/exemplopassagemvalorframe/JFTelaSecundaria.java
@@ -2,22 +2,44 @@ package exemplopassagemvalorframe;
 
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class JFTelaSecundaria extends JFrame {
     
     int pulo = 0;
-    public JFTelaSecundaria(int pulo) {
+    JSONArray asks;
+    public JFTelaSecundaria(int pulo, final JSONArray asks) {
         this.initComponents();
         this.pulo = pulo;
-        jLabel1.setText(String.valueOf(pulo));
+        this.asks = asks;
+        
+        for(int i = 0; i < this.asks.size(); i++){
+            final JSONObject jsonObject = (JSONObject) this.asks.get(i);
+            perguntas.setText((String) jsonObject.get("pergunta"));
+            final JSONArray aswers = (JSONArray) jsonObject.get("respostas");
+            respostaUm.setText((String) ((JSONObject) aswers.get(0)).get("resposta"));
+            respostaDois.setText((String) ((JSONObject) aswers.get(1)).get("resposta"));
+            respostaTres.setText((String) ((JSONObject) aswers.get(2)).get("resposta"));
+            respostaQuatro.setText((String) ((JSONObject) aswers.get(3)).get("resposta"));
+            this.asks.remove(i);
+            break;
+        }
+        
+        jLabel2.setText(String.valueOf(pulo));
     }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        perguntas = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        respostaUm = new javax.swing.JLabel();
+        respostaDois = new javax.swing.JLabel();
+        respostaTres = new javax.swing.JLabel();
+        respostaQuatro = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -33,26 +55,51 @@ public class JFTelaSecundaria extends JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(125, 125, 125)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(127, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(271, 271, 271))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(respostaUm, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(perguntas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(respostaDois, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(respostaQuatro, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(respostaTres, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(140, 140, 140)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGap(141, 141, 141)))
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(335, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(20, 20, 20)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(94, 94, 94)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(169, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(perguntas, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(respostaUm, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(respostaDois, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(respostaTres, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(respostaQuatro, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 87, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(138, 138, 138)
-                    .addComponent(jButton1)
-                    .addContainerGap(139, Short.MAX_VALUE)))
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(258, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(21, 21, 21)))
         );
 
         pack();
@@ -63,11 +110,16 @@ public class JFTelaSecundaria extends JFrame {
         if(pulo == 0) {
             this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));      
         }        
-        new JFTelaSecundaria(pulo).setVisible(true);
+        new JFTelaSecundaria(pulo, asks).setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel perguntas;
+    private javax.swing.JLabel respostaDois;
+    private javax.swing.JLabel respostaQuatro;
+    private javax.swing.JLabel respostaTres;
+    private javax.swing.JLabel respostaUm;
     // End of variables declaration//GEN-END:variables
 }
